f957c85da3a6ee7b81b0621b18083884915130dc
lilia <lilianom59@outlook.fr>
2023-01-12 14:01:11 +0100

    fsfse

--- index.php
+++ index.php
@@ -2,32 +2,37 @@
 
 // Premiere ligne
 
-$fruits = array("Apple", "Banana" "Orange");
-echo ;
+$fruits = array("Apple", "Banana", "Orange");
+echo count($fruits) . "<br>";
+echo $fruits[1];
 
 //Deuxieme ligne
 
 
-$age = array("Peter" "35", "Ben" "37", "Joe" "43");
+$age = array("Peter" => "35", "Ben" => "37", "Joe" => "43");
 
-echo "<br><br>Ben is " .  . " years old.<br><br>";
+echo "<br><br>Ben is " . $age["Ben"] . " years old.<br><br>";
 
-($age  $x  $y) {
-    echo "<br>Key=" .  . ", Value=" . ;
+foreach($age as $x => $y) {
+    echo "<br>Key=" . $x . ", Value=" . $y ;
 }
-
-
 
 //Troisieme ligne
 
 $colors = array("red", "green", "blue", "yellow");
-;
-
+sort($colors);
+foreach ($colors as $item) {
+    echo "$item<br>";
+}
 
 // Quatrieme ligne
 
 $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
-;
+asort($age);
+foreach ($age as $item => $value) {
+    echo "$value<br>";
+}
+
 
 
 
